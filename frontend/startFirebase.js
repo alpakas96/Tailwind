ede3fbc0b4a3fcc80d6ceb40a63f3567605dc518
alpakas96 <jhurleyfilm@gmail.com>
2023-03-14 14:21:39 -0400

    added process.env object to firebaseConfig

--- frontend/startFirebase.js
+++ frontend/startFirebase.js
@@ -1,25 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import * as dotenv from 'dotenv';
-dotenv.config();
 
-console.log("API_KEY:", process.env.REACT_APP_API_KEY);
-console.log("AUTH_DOMAIN:", process.env.REACT_APP_AUTH_DOMAIN);
-console.log("PROJECT_ID:", process.env.REACT_APP_PROJECT_ID);
-console.log("STORAGE_BUCKET:", process.env.REACT_APP_STORAGE_BUCKET);
-console.log("MESSAGING_SENDER_ID:", process.env.REACT_APP_MESSAGING_SENDER_ID);
-console.log("APP_ID:", process.env.REACT_APP_APP_ID);
-console.log("MEASUREMENT_ID:", process.env.REACT_APP_MEASUREMENT_ID);
+dotenv.config();
 
 export default function startFirebase() {
     const firebaseConfig = {
-        // apiKey: API_KEY,
-        // authDomain: AUTH_DOMAIN,
-        // projectId: PROJECT_ID,  
-        // storageBucket: STORAGE_BUCKET,
-        // messagingSenderId: MESSAGING_SENDER_ID,
-        // appId: APP_ID,
-        // measurementId: MEASUREMENT_ID
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_PROJECT_ID,  
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID
         
     };
     const database = getDatabase(initializeApp(firebaseConfig));
